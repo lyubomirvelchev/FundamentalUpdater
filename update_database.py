b52cb2e26cdd8b3e2895df0aa5818d010ab62535
lyubomirvelchev <lyubomirvelchev98@gmail.com>
2022-10-30 23:39:29 +0200

    Full commit

---
+++ update_database.py
@@ -0,0 +1,15 @@
+from updater import UpdateTables
+from project_constants import EXCHANGE_LIST_V1, EXCHANGE_LIST_V2, EXCHANGE_LIST_V3, EXCHANGE_LIST_V4
+
+
+def update_tables():
+    aws_connection = 'mysql+mysqlconnector://admin:dsj89jdj!li3dj2ljefds@database-1.clns6yopmify.us-east-1.rds.amazonaws.com:3306'
+    aws_database_name = 'assets'
+    UpdateTables(aws_connection, aws_database_name, EXCHANGE_LIST_V1)
+    UpdateTables(aws_connection, aws_database_name, EXCHANGE_LIST_V2)
+    UpdateTables(aws_connection, aws_database_name, EXCHANGE_LIST_V3)
+    UpdateTables(aws_connection, aws_database_name, EXCHANGE_LIST_V4)
+
+
+if __name__ == '__main__':
+    update_tables()
