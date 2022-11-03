854b1ff1c742f594e8b261702f9325c9c7307dd3
lyubomirvelchev <lyubomirvelchev98@gmail.com>
2022-11-03 15:43:37 +0200

    database name changed

--- update_database.py
+++ update_database.py
@@ -5,7 +5,7 @@ from project_constants import EXCHANGE_LIST_V1, EXCHANGE_LIST_V2, EXCHANGE_LIST_
 
 def update_tables():
     aws_connection = 'mysql+mysqlconnector://admin:dsj89jdj!li3dj2ljefds@database-1.clns6yopmify.us-east-1.rds.amazonaws.com:3306'
-    stocks_schema_name = 'assets'
+    stocks_schema_name = 'stock_etf_fundamentals'
     crypto_schema_name = 'crypto_fundamentals'
     UpdateCryptoTable(aws_connection, crypto_schema_name)
     UpdateTables(aws_connection, stocks_schema_name, EXCHANGE_LIST_V1)
